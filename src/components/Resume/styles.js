6cc54e1d0390cfc3314969d81891670c3ba03a6a
Karina_scalabrini <scalabrini0707@gmail.com>
2022-10-04 22:42:50 -0300

    projeto ainda em contrução

---
+++ src/components/Resume/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+max-width:1120px;
+width:98%;
+margin: 0 auto;
+display:flex;
+gap:60px;
+margin-top:-50px;
+justify-content: justify-content;
+`;
+
